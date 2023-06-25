6f2e3545f0575255108e6b3a396f6d0f05acdf55
raza-h <razah12145@gmail.com>
2023-06-25 16:55:06 +0500

    Buttons hover state changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,14 +10,14 @@ export default function Home() {
   }
 
   return (
-    <section id = "home" className = "bg text-[#5A5959] py-10 px-5 lg:flex lg:flex-col lg:justify-center">
+    <section id = "home" className = "bg text-[#5a5959] py-10 px-5 lg:flex lg:flex-col lg:justify-center">
       <figure className = " ml-auto mr-auto w-fit md:space-y-10 lg:grid lg:grid-cols-2 lg:justify-between lg:gap-x-20">
         <figcaption className='px-5 py-2'>
           <h1 className='text-2xl sm:text-3xl py-2 md:text-4xl lg:text-3xl xl:text-4xl'>Raza Haider</h1>
           <h2 className='text-4xl sm:text-5xl md:text-7xl lg:text-5xl xl:text-7xl'>Web Developer</h2>
           <div className='py-8 px-2'>
-            <Link onClick = {scrollDown} to = "projects"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto block sm:inline shadow-md lg:text-lg xl:text-xl border-2 hover:bg-[#cac7bf] transition-all border-[#5a5959] px-2 py-1 sm:mr-1 rounded-sm">View Projects</button></Link>
-            <Link onClick = {scrollDown} to = "contact"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto my-3 sm:my-0 block sm:inline shadow-md  shadow-[#5a5959aa] lg:text-lg xl:text-xl border-2 border-[#5a5959] hover:bg-[#5a5961] hover:border-[#5a595961] bg-[#5a5959] text-[#c1c7bd] px-3 py-1 sm:ml-1 rounded-sm transition-all">Get in touch</button></Link>
+            <Link onClick = {scrollDown} to = "projects"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto block sm:inline shadow-md lg:text-lg xl:text-xl border-2 hover:bg-[#c1c7c1] transition-all border-[#5a5959] px-2 py-1 sm:mr-1 rounded-sm">View Projects</button></Link>
+            <Link onClick = {scrollDown} to = "contact"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto my-3 sm:my-0 block sm:inline shadow-md  shadow-[#5a5959aa] lg:text-lg xl:text-xl border-2 border-[#5a5959] hover:bg-[#5a5959ee] hover:border-[#5a595961] bg-[#5a5959] text-[#c1c7bd] px-3 py-1 sm:ml-1 rounded-sm transition-all">Get in touch</button></Link>
           </div>
         </figcaption>
         <img alt = "webpage" src = {webpageImg} width = "500rem" />
