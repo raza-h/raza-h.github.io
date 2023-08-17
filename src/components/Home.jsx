87c2d7917fc2a3301aa8bd6b7d5a4920c2021b40
raza-h <razah12145@gmail.com>
2023-08-17 16:22:11 +0500

    Tech Stack added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,18 +16,18 @@ export default function Home() {
       <LinkHeader />
       <hr className='border-[#5a5959]'/>
       <main id = "home" className = "bg text-[#5a5959] py-10 px-5 lg:flex lg:flex-col lg:justify-around">
-        <figure className = " mx-auto w-fit md:space-y-10 lg:grid lg:grid-cols-2 lg:justify-between lg:gap-x-20">
+        <figure className = "mx-auto w-fit md:space-y-10 lg:grid lg:grid-cols-2 lg:justify-between lg:gap-x-20">
           <figcaption className='px-5'>
             <h1 className='text-2xl sm:text-3xl py-2 md:text-3xl lg:text-2xl xl:text-4xl'>Raza Haider</h1>
-            <h2 className='text-4xl sm:text-5xl md:text-7xl lg:text-5xl xl:text-7xl'>Web Developer</h2>
+            <h2 className='text-4xl sm:text-6xl md:text-7xl lg:text-5xl xl:text-7xl'>Web Developer</h2>
             <div className='py-8 px-2'>
               <Link onClick = {scrollDown} to = "projects"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto block sm:inline shadow-md lg:text-lg xl:text-xl border-2 hover:bg-[#cac7c1] transition-all border-[#5a5959] px-2 py-1 sm:mr-1 rounded-sm">View Projects</button></Link>
               <Link onClick = {scrollDown} to = "contact"><button className = "w-[70vw] sm:w-fit ml-auto mr-auto my-3 sm:my-0 block sm:inline shadow-md  shadow-[#5a5959aa] lg:text-lg xl:text-xl border-2 border-[#5a5959] hover:bg-[#5a5959ee] hover:border-[#5a595961] bg-[#5a5959] text-[#cac7bd] px-3 py-1 sm:ml-1 rounded-sm transition-all">Get in touch</button></Link>
             </div>
           </figcaption>
-          <img alt = "webpage" className="mx-auto" src = {webpageImg} width = "500rem" />
+          <img alt = "webpage" className="mx-auto mt-5" src = {webpageImg} width = "500rem" />
         </figure>
-        <a href = "#next" className='py-1'><button className = "block ml-auto mr-auto w-fit my-5 shadow-md rounded-3xl border-2 border-[#5a5959]"><img alt = "down icon" src = {downIcon} width = "40rem"/></button></a>
+        <a href = "#next" className='py-1'><button className = "expand-reduce block ml-auto mr-auto w-fit my-5 shadow-md rounded-3xl border-2 border-[#5a5959]"><img alt = "down icon" src = {downIcon} width = "40rem"/></button></a>
       </main>
     </>
   )
--- src/components/subcomponents/LinkHeader.jsx
+++ src/components/subcomponents/LinkHeader.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import linkedin from '../../assets/images/linkedin.svg'
 import github from '../../assets/images/github.svg'
 import fast from '../../assets/images/fast.svg'
+import react from '../../assets/images/react.svg'
+import node from '../../assets/images/node.svg'
+import dotnet from '../../assets/images/dotnet.svg'
+import tailwind from '../../assets/images/tailwind.svg'
+import ts from '../../assets/images/typescript.svg'
+import spring from '../../assets/images/spring.svg'
 
 const LinkHeader = () => {
   return (
-    <header id="top" className='bg-[#cac7bd] px-5 lg:py-5'>
-    <ul className='flex justify-end space-between w-[100%]'>
-        <li className='flex mr-auto'>
+    <header id="top" className='bg-[#cac7bd] px-2 sm:px-3.5 md:px-5 lg:py-5'>
+    <ul className='flex items-center justify-between w-[100%]'>
+        <li className='flex'>
             <a rel="noreferrer" target="_blank" href="https://lhr.nu.edu.pk"><img src = {fast} width="51rem" alt="university link"/></a>
         </li>
-        <li className='flex items-center space-x-4'>
-            <a rel="noreferrer" target="_blank" href="https://github.com/dork-writes"><img src = {github} width="25rem" alt = "github link"/></a>
-            <a rel="noreferrer" target="_blank" href="https://linkedin.com/in/razah1214"><img src = {linkedin} width="25rem" alt="linkedin link"/></a>
+        <li className='flex space-x-1.5 sm:space-x-2.5 md:space-x-5'>
+          <img src = {react} width={'30rem'} alt = "react icon"/>
+          <img src = {tailwind} width={'30rem'} alt = "tailwind icon"/>
+          <img src = {ts} width={'30rem'} alt = "typesript icon"/>
+          <img src = {node} width={'30rem'} alt = "node icon"/>
+          <img src = {dotnet} width={'35rem'} alt = "dotnet icon"/> 
+          <img src = {spring} width={'26rem'} alt = "spring icon"/> 
+        </li>
+        <li className='flex items-center space-x-1.5 sm:space-x-2 md:space-x-4'>
+            <a rel="noreferrer" target="_blank" href="https://github.com/dork-writes"><img src = {github} width="30rem" alt = "github link"/></a>
+            <a rel="noreferrer" target="_blank" href="https://linkedin.com/in/razah1214"><img src = {linkedin} width="30rem" alt="linkedin link"/></a>
         </li>
     </ul>
     </header>
