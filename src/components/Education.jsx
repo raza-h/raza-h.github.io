999998bbfe50ae07921d11bad5aee9470398f02a
Raza Haider <razah12145@gmail.com>
2023-09-11 04:52:11 +0500

    Cross added to Education layover to close on touch screens.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,6 +6,7 @@ const Education = ({ visibility, setVisible }) => {
 
   return (
     <aside onMouseLeave={() => setVisible(false)} className={`bg-[#5a5959] text-[#cac7bd] absolute z-20 space-y-4 overflow-hidden shadow-xl rounded-md top-0 ${size} m-1 transition-all origin-top-left`}>
+      <button className="block text-sm" onClick={() => setVisible(false)}>X</button>
       <article className='space-y-1'>
         <h2 className='text-sm text-right'>Lahore, Pakistan</h2>
         <h1 className='text-2xl'>FAST - NUCES</h1>
